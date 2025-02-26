44e8eff969b71739312497a2efc59972f9510d38
Charlie Parker <charlie.parker@ideacrew.com>
2025-02-26 15:22:58 -0500

    add tabindex and keydown handlers for clickable tables (#5052)
    * add tabindex and keydown handlers for navigateable table
    
    * make expandable navigateable as well
    
    * remove import

--- app/javascript/controllers/expandable_table_controller.js
+++ app/javascript/controllers/expandable_table_controller.js
@@ -10,15 +10,29 @@ export default class extends Controller {
   connect() {
     // prepend a caret icon to the first cell of each row
     this.rowTargets.forEach(row => {
-      if(!this._isExpandable(row)) { return; }
+      if(!this.#isExpandable(row)) { return; }
 
-      row.setAttribute('data-action', 'click->expandable-table#toggle');
+      const actions = [ "click", "keydown" ].map((eventType) => { return `${eventType}->${this.identifier}#handleEvent` }).join(" ");
+      row.setAttribute('data-action', actions);
+      row.setAttribute('tabindex', '0');
 
       const firstCell = row.querySelector('td');
+      const originalContent = Array.from(firstCell.children)
+  
       const caretDiv = document.createElement('div');
       caretDiv.classList.add('caret-icon', 'mr-2');
-      firstCell.insertBefore(caretDiv, firstCell.firstChild);
-      firstCell.classList.add('d-flex', 'align-items-center');
+
+      const newContent = document.createElement('div');
+      newContent.classList.add('d-flex', 'align-items-center');
+      newContent.appendChild(caretDiv);
+      originalContent.forEach(child => {
+        newContent.appendChild(child);
+      });
+
+      while (firstCell.firstChild) {
+        firstCell.removeChild(firstCell.firstChild);
+      }
+      firstCell.appendChild(newContent);
     });
 
     // hide and prevent hover of detail rows
@@ -27,11 +41,15 @@ export default class extends Controller {
     });
   }
 
-  // Expand or collapse action which toggles the "expanded" class on the row element and the "hidden" class on the details element.
-  toggle(event) {    
-    const row = event.currentTarget
+  handleEvent(event) {
+    if (event.type === "click" || (event.type === "keydown" && event.key === "Enter")) {
+      this.#toggle(event.currentTarget);
+     }
+  }
 
-    if (!this._isExpandable(row) || window.getSelection().toString()) { return; } // prevent click fires when the row isn't expandable or the user was highlighting the cell
+  // Expand or collapse action which toggles the "expanded" class on the row element and the "hidden" class on the details element.
+  #toggle(row) {    
+    if (!this.#isExpandable(row) || window.getSelection().toString()) { return; } // prevent click fires when the row isn't expandable or the user was highlighting the cell
 
     const details = row.nextElementSibling;
 
@@ -46,7 +64,7 @@ export default class extends Controller {
   * @param {HTMLElement} row - The row element to check.
   * @returns {boolean} - Returns true if the row is expandable, otherwise false.
   */
-  _isExpandable(row) {
+  #isExpandable(row) {
     const nextRow = row.nextElementSibling;
     return nextRow && nextRow.dataset.target === "expandable-table.details";
   }
--- app/javascript/controllers/navigateable_table_controller.js
+++ app/javascript/controllers/navigateable_table_controller.js
@@ -3,12 +3,20 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   connect() {
     this.element.querySelectorAll("tr[data-href]").forEach((row) => {
-      row.setAttribute("data-action", `click->${this.identifier}#navigate`);
+      const actions = [ "click", "keydown" ].map((eventType) => { return `${eventType}->${this.identifier}#handleEvent` }).join(" ");
+      row.setAttribute("data-action", actions);
+      row.setAttribute("tabindex", "0");
     });
   }
 
-  navigate(event) {
-    const url = event.currentTarget.dataset.href;
+  handleEvent(event) {
+    if (event.type === "click" || (event.type === "keydown" && event.key === "Enter")) {
+      this.#navigate(event.currentTarget);
+    }
+  }
+
+  #navigate(target) {
+    const url = target.dataset.href;
     if (url) {
       window.location.href = url;
     }
