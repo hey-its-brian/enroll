89dc9ab3f700204b57f7708fd9343827058e4ef0
Charlie Parker <charlie.parker@ideacrew.com>
2025-02-27 13:46:43 -0500

    fix verifications member sort order (#5056)
    * use multiple sort conditions
    
    * use na for first table date

--- app/controllers/insured/families_controller.rb
+++ app/controllers/insured/families_controller.rb
@@ -239,10 +239,8 @@ class Insured::FamiliesController < FamiliesController
       @action_items = @family.eligibility_determination.subjects.map { |subject| subject.eligibility_states.by_type_uploadable }.flatten.map { |state| state.evidence_states.where_action_needed }.flatten.sort_by(&:due_on)
       @action_items = @action_items.map { |evidence| EvidenceStateDecorator.new(evidence) }
 
-      # sort subjects first by `documents_outstanding?`, then by `earliest_due_date`, then by nil `earliest_due_date``
-      subjects_with_dates, subjects_without_dates = @family.eligibility_determination.subjects.partition(&:earliest_due_date)
-      subjects_needing_action, subjects_not_needing_action = subjects_with_dates.partition(&:documents_outstanding?)
-      @subjects = subjects_needing_action.sort_by(&:earliest_due_date) + subjects_not_needing_action.sort_by(&:earliest_due_date) + subjects_without_dates
+      # sort subjects first by `documents_outstanding?`, then by `documents_outstanding?` then by nil `due_on``
+      @subjects = @family.eligibility_determination.subjects.sort_by { |subject| [subject.documents_outstanding? ? 0 : 1, subject.earliest_due_date || Float::INFINITY] }
     else
       @family_members = @person.primary_family.has_active_consumer_family_members
     end
@@ -258,10 +256,8 @@ class Insured::FamiliesController < FamiliesController
 
     # sort evidences first by `is_action_needed?`, then by `due_on`, then by nil `due_on``
     evidences = subject.eligibility_states.by_type_uploadable.map(&:evidence_states).flatten
-    valid_dates, nil_dates = evidences.partition(&:due_on)
-    action_needed, no_action_needed = valid_dates.partition(&:is_action_needed?)
-    sorted_evidence_states = action_needed.sort_by(&:due_on) + no_action_needed.sort_by(&:due_on) + nil_dates
-    @evidences = sorted_evidence_states.map { |evidence| EvidenceStateDecorator.new(evidence) }
+    sorted_evidences = evidences.sort_by { |evidence| [evidence.is_action_needed? ? 0 : 1, evidence.due_on || Float::INFINITY] }
+    @evidences = sorted_evidences.map { |evidence| EvidenceStateDecorator.new(evidence) }
 
     respond_to :html
   end
