c94367363e7484026d8e52b0d40ff5ca4182a99d
Charlie Parker <charlie.parker@ideacrew.com>
2025-04-11 14:50:13 -0400

    fix date reference in history ui (#5151)
    * update history field of evidence decorator to use date_of_action for evidences/created_at for verifications
    
    * restore decorator
    
    ---------
    
    Co-authored-by: Tristan <33517218+TristanB17@users.noreply.github.com>

--- app/models/evidence_history_decorator.rb
+++ app/models/evidence_history_decorator.rb
@@ -5,7 +5,7 @@ class EvidenceHistoryDecorator < SimpleDelegator
   include L10nHelper
   include VerificationHelper
 
-  attr_reader :action, :modifier_id, :update_reason, :payload
+  attr_reader :action, :modifier_id, :update_reason, :date_of_action, :payload
 
   def initialize(obj)
     super(obj)
@@ -14,17 +14,20 @@ class EvidenceHistoryDecorator < SimpleDelegator
       @action = obj.action
       @modifier_id = obj.modifier
       @update_reason = obj.update_reason
+      @date_of_action = obj.created_at
       verification_type = obj.verification_type
       @payload = request_response_details_formatted(verification_type.person, obj, verification_type.type_name) if obj.event_request_record_id.present? || obj.event_response_record_id.present?
     when Eligibilities::RequestResult
       @action = obj.action || l10n('hub_call')
       @modifier_id = obj.source_transaction_id
       @update_reason = l10n('fdsh_hub_call')
+      @date_of_action = obj.date_of_action
       @payload = JSON.parse(obj.raw_payload) if obj.raw_payload.present?
     when Eligibilities::VerificationHistory
       @action = obj.action
       @modifier_id = obj.updated_by
       @update_reason = obj.update_reason
+      @date_of_action = obj.date_of_action
       @payload = nil
     end
   end
