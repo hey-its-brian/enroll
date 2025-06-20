a34c13db4d05a1f81fd928fe3161eb5b56f92a80
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2025-06-20 17:34:02 -0400

    updates agree, disagree texts on preferences page (#5349)
    * updates agree, disagree texts on preferences page
    
    * fixes Psych error on review and submit page

---
+++ db/seedfiles/translations/en/ic/insured.rb
@@ -0,0 +1,6 @@
+# IC Insured Translations
+
+INSURED_TRANSLATIONS = {
+  :'en.qhp_application.preferences.i_agree' => 'I agree',
+  :'en.qhp_application.preferences.i_disagree' => 'I disagree'
+}.freeze
