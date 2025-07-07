eb838fffa06bb51b4804295b82db7283b62e80e9
kristinmerbach <78749008+kristinmerbach@users.noreply.github.com>
2025-07-07 12:43:57 -0700

    log out links/buttons should trigger leaving modal (#5405)
    Co-authored-by: haridhar yamjala <hariym471@gmail.com>
    Co-authored-by: quisbertdiana <82827623+quisbertdiana@users.noreply.github.com>

--- app/javascript/controllers/leaving_modal_controller.js
+++ app/javascript/controllers/leaving_modal_controller.js
@@ -7,19 +7,31 @@ export default class extends Controller {
   }
 
   modalWhenLeavingFormUnfinished() {
+    const headerLogOutLink = document.querySelector('header a[data-method="delete"]')
+    headerLogOutLink.removeAttribute('data-method')
     const headerLinks = Array.from(document.querySelectorAll('header a, .progress-nav-container a')).filter(link => !link.closest('.progress-nav')).filter(link => !link.closest('.modal')).filter(link => !link.dataset.target).filter(link => link.href.length > 1)
     headerLinks.forEach(link => {
       link.addEventListener('click', (e) => {
         e.preventDefault()
-        this.showLeavingModal(link.href)
+        this.showLeavingModal(link.href, headerLogOutLink)
+      })
+    })
+    const buttons = Array.from(document.querySelectorAll('header button, .progress-nav-container button')).filter(button => !button.closest('.progress-nav')).filter(button => !button.closest('.modal')).filter(button => !button.dataset.target)
+    buttons.forEach(button => {
+      button.addEventListener('click', (e) => {
+        e.preventDefault()
+        this.showLeavingModal(button.href, headerLogOutLink)
       })
     })
   }
 
-  showLeavingModal(href) {
+  showLeavingModal(href, headerLogOutLink) {
     const leavingModalTrigger = document.getElementById('leavingWarningModalTrigger')
     leavingModalTrigger.click()
     this.leavingApplicationButtonTarget.addEventListener('click', () => {
+      if (this.leavingApplicationButtonTarget.textContent === 'Logout') {
+        headerLogOutLink.setAttribute('data-method', 'delete')
+      }
       window.location.href = href
     })
   }
