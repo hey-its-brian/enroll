90bf292d4f50a7f0d40a9a14cf10f08113988fbe
Alec Turnbull <alecturnbull81@gmail.com>
2026-01-09 13:24:38 -0600

    add additional address updates for applications (#5979)
    * add additional address updates for apps
    
    * don't update determined apps

--- app/data_migrations/update_missing_county.rb
+++ app/data_migrations/update_missing_county.rb
@@ -2,9 +2,23 @@
 
 require File.join(Rails.root, "lib/mongoid_migration_task")
 
-#Rake to update invalid counties in person addresses based on zip code when they should be present
+#Rake to update invalid counties in person addresses, Financial Assistance applicant addresses,
+# and Individual marketplace applicant addresses based on zip code when they should be present
 class UpdateMissingCounty < MongoidMigrationTask
   def migrate
+    # Update Person addresses
+    update_person_addresses
+
+    # Update Financial Assistance applicant addresses
+    update_financial_assistance_applicant_addresses
+
+    # Update Individual marketplace applicant addresses
+    update_individual_market_applicant_addresses
+  end
+
+  private
+
+  def update_person_addresses
     people_with_invalid_county = Person.where(
       "addresses.county" => "Please provide a zip code",
       "addresses.zip" => { "$ne" => nil }
@@ -14,12 +28,53 @@ class UpdateMissingCounty < MongoidMigrationTask
       person.addresses.each do |address|
         next if address.county != "Please provide a zip code" && address.zip.nil?
         county_zip = ::BenefitMarkets::Locations::CountyZip.where(:zip => address.zip, :state => address.state).last
-        if county_zip&.county_name.present?
+        next unless county_zip&.county_name.present?
+        address.county = county_zip.county_name
+        address.save!
+        puts "Person #{person.hbx_id} - Address updated with county: #{county_zip.county_name}"
+      end
+    end
+  end
+
+  def update_financial_assistance_applicant_addresses
+    fa_applications_with_invalid_county = ::FinancialAssistance::Application.where(
+      "applicants.addresses.county" => "Please provide a zip code",
+      "applicants.addresses.zip" => { "$ne" => nil },
+      :assistance_year.in => [2025, 2026],
+      :aasm_state.ne => "determined"
+    )
+
+    fa_applications_with_invalid_county.each do |application|
+      application.applicants.each do |applicant|
+        applicant.addresses.each do |address|
+          next if address.county != "Please provide a zip code" && address.zip.nil?
+          county_zip = ::BenefitMarkets::Locations::CountyZip.where(:zip => address.zip, :state => address.state).last
+          next unless county_zip&.county_name.present?
           address.county = county_zip.county_name
           address.save!
-          puts "Person #{person.hbx_id} - Address updated with county: #{county_zip.county_name}"
-        else
-          puts "Person #{person.hbx_id} - No county found for zip: #{address.zip}, state: #{address.state}"
+          puts "FA Application #{application.hbx_id} - Applicant #{applicant.person_hbx_id} - Address updated with county: #{county_zip.county_name}"
+        end
+      end
+    end
+  end
+
+  def update_individual_market_applicant_addresses
+    im_applications_with_invalid_county = ::IndividualMarket::Application.where(
+      "applicants.addresses.county" => "Please provide a zip code",
+      "applicants.addresses.zip" => { "$ne" => nil },
+      :assistance_year.in => [2025, 2026],
+      :current_state.ne => :determined
+    )
+
+    im_applications_with_invalid_county.each do |application|
+      application.applicants.each do |applicant|
+        applicant.addresses.each do |address|
+          next if address.county != "Please provide a zip code" && address.zip.nil?
+          county_zip = ::BenefitMarkets::Locations::CountyZip.where(:zip => address.zip, :state => address.state).last
+          next unless county_zip&.county_name.present?
+          address.county = county_zip.county_name
+          address.save!
+          puts "IM Application #{application.hbx_id} - Applicant #{applicant.hbx_id} - Address updated with county: #{county_zip.county_name}"
         end
       end
     end
--- spec/data_migrations/update_missing_county_spec.rb
+++ spec/data_migrations/update_missing_county_spec.rb
@@ -47,8 +47,7 @@ describe UpdateMissingCounty do
       end
 
       it "does not update the county and logs message" do
-        expect { subject.migrate }.to output(/Person #{person.hbx_id} - No county found for zip: 99999, state: ME/).to_stdout
-
+        subject.migrate
         person.reload
         expect(person.addresses.first.county).to eq "Please provide a zip code"
       end
@@ -104,9 +103,7 @@ describe UpdateMissingCounty do
       end
 
       it "updates only the address with valid county zip" do
-        expect { subject.migrate }.to output(
-          /Person #{person.hbx_id} - Address updated with county: Valid County.*Person #{person.hbx_id} - No county found for zip: 99999, state: ME/m
-        ).to_stdout
+        expect { subject.migrate }.to output(/Person #{person.hbx_id} - Address updated with county: Valid County/).to_stdout
 
         person.reload
         expect(person.addresses.first.county).to eq "Valid County"
@@ -127,10 +124,236 @@ describe UpdateMissingCounty do
       end
 
       it "does not update the county" do
-        expect { subject.migrate }.to output(/Person #{person.hbx_id} - No county found for zip: 11111, state: ME/).to_stdout
-
+        subject.migrate
         person.reload
         expect(person.addresses.first.county).to eq "Please provide a zip code"
+      end
+    end
+
+    context "when financial assistance application has invalid county with valid zip" do
+      let!(:fa_application) do
+        FactoryBot.create(:financial_assistance_application, assistance_year: 2025, aasm_state: "draft").tap do |application|
+          applicant = FactoryBot.build(:applicant, application: application, person_hbx_id: "test_hbx_id_fa")
+          address = FactoryBot.build(:financial_assistance_address, county: "Please provide a zip code", zip: "12345", state: "ME")
+          applicant.addresses = [address]
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "updates the financial assistance applicant address county" do
+        expect { subject.migrate }.to output(/FA Application #{fa_application.hbx_id} - Applicant test_hbx_id_fa - Address updated with county: Valid County/).to_stdout
+
+        fa_application.reload
+        expect(fa_application.applicants.first.addresses.first.county).to eq "Valid County"
+      end
+    end
+
+    context "when financial assistance application has invalid county but no corresponding county zip record" do
+      let!(:fa_application) do
+        FactoryBot.create(:financial_assistance_application, assistance_year: 2025, aasm_state: "draft").tap do |application|
+          applicant = FactoryBot.build(:applicant, application: application, person_hbx_id: "test_hbx_id_fa_invalid")
+          address = FactoryBot.build(:financial_assistance_address, county: "Please provide a zip code", zip: "99999", state: "ME")
+          applicant.addresses = [address]
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "does not update the county and logs message" do
+        subject.migrate
+        fa_application.reload
+        expect(fa_application.applicants.first.addresses.first.county).to eq "Please provide a zip code"
+      end
+    end
+
+    context "when financial assistance application is from non-target year" do
+      let!(:fa_application) do
+        FactoryBot.create(:financial_assistance_application, assistance_year: 2024, aasm_state: "draft").tap do |application|
+          applicant = FactoryBot.build(:applicant, application: application, person_hbx_id: "test_hbx_id_fa_2024")
+          address = FactoryBot.build(:financial_assistance_address, county: "Please provide a zip code", zip: "12345", state: "ME")
+          applicant.addresses = [address]
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "does not update the county" do
+        subject.migrate
+        fa_application.reload
+        expect(fa_application.applicants.first.addresses.first.county).to eq "Please provide a zip code"
+      end
+    end
+
+    context "when individual market application has invalid county with valid zip" do
+      let!(:im_application) do
+        FactoryBot.create(:individual_market_application, assistance_year: 2026, current_state: :initial).tap do |application|
+          applicant = FactoryBot.build(:individual_market_applicant, :with_home_address, application: application, hbx_id: "test_hbx_id_im")
+          applicant.addresses.first.county = "Please provide a zip code"
+          applicant.addresses.first.zip = "12345"
+          applicant.addresses.first.state = "ME"
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "updates the individual market applicant address county" do
+        expect { subject.migrate }.to output(/IM Application #{im_application.hbx_id} - Applicant test_hbx_id_im - Address updated with county: Valid County/).to_stdout
+
+        im_application.reload
+        expect(im_application.applicants.first.addresses.first.county).to eq "Valid County"
+      end
+    end
+
+    context "when individual market application has invalid county but no corresponding county zip record" do
+      let!(:im_application) do
+        FactoryBot.create(:individual_market_application, assistance_year: 2025, current_state: :initial).tap do |application|
+          applicant = FactoryBot.build(:individual_market_applicant, :with_home_address, application: application, hbx_id: "test_hbx_id_im_invalid")
+          applicant.addresses.first.county = "Please provide a zip code"
+          applicant.addresses.first.zip = "99999"
+          applicant.addresses.first.state = "ME"
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "does not update the county and logs message" do
+        subject.migrate
+        im_application.reload
+        expect(im_application.applicants.first.addresses.first.county).to eq "Please provide a zip code"
+      end
+    end
+
+    context "when individual market application is from non-target year" do
+      let!(:im_application) do
+        FactoryBot.create(:individual_market_application, assistance_year: 2024, current_state: :initial).tap do |application|
+          applicant = FactoryBot.build(:individual_market_applicant, :with_home_address, application: application, hbx_id: "test_hbx_id_im_2024")
+          applicant.addresses.first.county = "Please provide a zip code"
+          applicant.addresses.first.zip = "12345"
+          applicant.addresses.first.state = "ME"
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "does not update the county" do
+        subject.migrate
+        im_application.reload
+        expect(im_application.applicants.first.addresses.first.county).to eq "Please provide a zip code"
+      end
+    end
+
+    context "when multiple applications have mixed scenarios" do
+      let!(:person) do
+        FactoryBot.create(:person).tap do |person|
+          person.addresses.first.update!(
+            county: "Please provide a zip code",
+            zip: "12345",
+            state: "ME"
+          )
+        end
+      end
+
+      let!(:fa_application) do
+        FactoryBot.create(:financial_assistance_application, assistance_year: 2025, aasm_state: "draft").tap do |application|
+          applicant = FactoryBot.build(:applicant, application: application, person_hbx_id: "test_hbx_id_mixed_fa")
+          address = FactoryBot.build(:financial_assistance_address, county: "Please provide a zip code", zip: "99999", state: "ME")
+          applicant.addresses = [address]
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      let!(:im_application) do
+        FactoryBot.create(:individual_market_application, assistance_year: 2025, current_state: :initial).tap do |application|
+          applicant = FactoryBot.build(:individual_market_applicant, :with_home_address, application: application, hbx_id: "test_hbx_id_mixed_im")
+          applicant.addresses.first.county = "Please provide a zip code"
+          applicant.addresses.first.zip = "12345"
+          applicant.addresses.first.state = "ME"
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "processes all applications and updates only valid ones" do
+        expect { subject.migrate }.to output(/Person #{person.hbx_id} - Address updated with county: Valid County.*IM Application #{im_application.hbx_id} - Applicant test_hbx_id_mixed_im - Address updated with county: Valid County/m).to_stdout
+
+        person.reload
+        fa_application.reload
+        im_application.reload
+
+        expect(person.addresses.first.county).to eq "Valid County"
+        expect(fa_application.applicants.first.addresses.first.county).to eq "Please provide a zip code"
+        expect(im_application.applicants.first.addresses.first.county).to eq "Valid County"
+      end
+    end
+
+    context "when applications have multiple applicants with different address scenarios" do
+      let!(:fa_application) do
+        FactoryBot.create(:financial_assistance_application, assistance_year: 2026, aasm_state: "draft").tap do |application|
+          # Primary applicant with valid county zip
+          primary_applicant = FactoryBot.build(:applicant, application: application, person_hbx_id: "primary_fa", is_primary_applicant: true)
+          primary_address = FactoryBot.build(:financial_assistance_address, county: "Please provide a zip code", zip: "12345", state: "ME")
+          primary_applicant.addresses = [primary_address]
+
+          # Dependent applicant with invalid county zip
+          dependent_applicant = FactoryBot.build(:applicant, application: application, person_hbx_id: "dependent_fa", is_primary_applicant: false)
+          dependent_address = FactoryBot.build(:financial_assistance_address, county: "Please provide a zip code", zip: "99999", state: "ME")
+          dependent_applicant.addresses = [dependent_address]
+
+          application.applicants = [primary_applicant, dependent_applicant]
+          application.save!
+        end
+      end
+
+      it "updates only the applicant with valid county zip" do
+        expect { subject.migrate }.to output(/FA Application #{fa_application.hbx_id} - Applicant primary_fa - Address updated with county: Valid County/).to_stdout
+
+        fa_application.reload
+        primary_applicant = fa_application.applicants.find { |a| a.person_hbx_id == "primary_fa" }
+        dependent_applicant = fa_application.applicants.find { |a| a.person_hbx_id == "dependent_fa" }
+
+        expect(primary_applicant.addresses.first.county).to eq "Valid County"
+        expect(dependent_applicant.addresses.first.county).to eq "Please provide a zip code"
+      end
+    end
+
+    context "when financial assistance application is in determined state" do
+      let!(:fa_application_determined) do
+        FactoryBot.create(:financial_assistance_application, assistance_year: 2025, aasm_state: "determined").tap do |application|
+          applicant = FactoryBot.build(:applicant, application: application, person_hbx_id: "determined_fa")
+          address = FactoryBot.build(:financial_assistance_address, county: "Please provide a zip code", zip: "12345", state: "ME")
+          applicant.addresses = [address]
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "does not update the county for determined applications" do
+        expect { subject.migrate }.not_to output(/FA Application #{fa_application_determined.hbx_id}/).to_stdout
+
+        fa_application_determined.reload
+        expect(fa_application_determined.applicants.first.addresses.first.county).to eq "Please provide a zip code"
+      end
+    end
+
+    context "when individual market application is in determined state" do
+      let!(:im_application_determined) do
+        FactoryBot.create(:individual_market_application, assistance_year: 2025, current_state: :determined).tap do |application|
+          applicant = FactoryBot.build(:individual_market_applicant, :with_home_address, application: application, hbx_id: "determined_im")
+          applicant.addresses.first.county = "Please provide a zip code"
+          applicant.addresses.first.zip = "12345"
+          applicant.addresses.first.state = "ME"
+          application.applicants = [applicant]
+          application.save!
+        end
+      end
+
+      it "does not update the county for determined applications" do
+        expect { subject.migrate }.not_to output(/IM Application #{im_application_determined.hbx_id}/).to_stdout
+
+        im_application_determined.reload
+        expect(im_application_determined.applicants.first.addresses.first.county).to eq "Please provide a zip code"
       end
     end
   end
